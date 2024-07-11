a5d4434f6d5a383c332e7577ef7ef3c913cc996a
andrearomeo04 <pisco004@gmail.com>
2024-07-12 01:13:58 +0200

    [UPD] called in the js the first child of images elementand declared a variable

--- js/script.js
+++ js/script.js
@@ -21,3 +21,8 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const image = document.querySelector('.images')
+
+let show_img = '';
+
